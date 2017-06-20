60f424e6111ca1869a147c0ba910bd6aec104868
Edgar Tamarit <edgar_pego@hotmail.com>
2017-06-20 20:56:43 +0200

    Improve the performance
    1 - Added progress indicator while the data is loading from the server.
    2 - The Refresh button is disabled until the data is loaded.
    3 - Code refactorizations.
    3.1 - Deleted the RootLayout.xml. Now the WidgetView.xml is the root
    view.
    3.2 - Added trayicon mouse click listener. The single click come it to
    the front and double click minimezes or maximizes the widget to the
    trayicon.
    3.3 - Some exceptions managed in order to avoid unexpected errors.

--- src/main/java/es/tamarit/widgetemt/core/MainApp.java
+++ src/main/java/es/tamarit/widgetemt/core/MainApp.java
@@ -23,219 +23,236 @@ import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class MainApp extends Application {
-
-    private static final Logger LOGGER = LogManager.getLogger(MainApp.class.getName());
-    public static final String URL_IMG_APP_16 = "/images/icon.png";
-
-    private Stage primaryStage;
-    private Stage secondaryStage;
-    private BorderPane rootLayout;
-    private Scene scene;
-    private Properties properties;
-    private TrayIcon trayIcon;
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        LOGGER.info("Application starting");
-
-        //Add tryIcon
-        createTrayIcon(primaryStage);
-
-        this.primaryStage = primaryStage;
-        this.secondaryStage = new Stage();
-
-        primaryStage.setTitle("EMT Widget");
-        primaryStage.initStyle(StageStyle.UTILITY);
-        primaryStage.setOpacity(0);
-        primaryStage.setWidth(1);
-        primaryStage.setHeight(1);
-
-        initRootLayout();
-
-        scene = new Scene(rootLayout);
-
-        loadStyleSheets();
-        initMainView();
-
-        secondaryStage.setScene(scene);
-        secondaryStage.initStyle(StageStyle.TRANSPARENT);
-        secondaryStage.initOwner(primaryStage);
-
-        primaryStage.show();
-        secondaryStage.show();
-    }
-
-    private void initRootLayout() {
-        try {
-            rootLayout = FXMLLoader.load(MainApp.class.getResource("/views/rootlayout/RootLayout.fxml"));
-        } catch (IOException e) {
-            LOGGER.error("Error loading the rootLayout", e);
-        }
-    }
-
-    private void loadStyleSheets() {
-
-        String[] styleSheets = { "/css/style.css", };
-        scene.getStylesheets().addAll(styleSheets);
-    }
-
-    private void initMainView() {
-        try {
-            AnchorPane mainView = FXMLLoader.load(MainApp.class.getResource("/views/widget/WidgetView.fxml"));
-            rootLayout.setCenter(mainView);
-
-            properties = new Properties();
-            InputStream inputStream = getClass().getResourceAsStream("/properties/application.properties");
-            properties.load(inputStream);
-            inputStream.close();
-
-            getPrimatyStage().setX(Integer.valueOf(properties.getProperty("widget.position.x")));
-            getPrimatyStage().setY(Integer.valueOf(properties.getProperty("widget.position.y")));
-            secondaryStage.setX(Integer.valueOf(properties.getProperty("widget.position.x")));
-            secondaryStage.setY(Integer.valueOf(properties.getProperty("widget.position.y")));
-
-            final Delta dragDelta = new Delta();
-            rootLayout.setOnMousePressed(new EventHandler<MouseEvent>() {
-
-                @Override
-                public void handle(MouseEvent mouseEvent) {
-                    // record a delta distance for the drag and drop operation.
-                    dragDelta.x = secondaryStage.getX() - mouseEvent.getScreenX();
-                    dragDelta.y = secondaryStage.getY() - mouseEvent.getScreenY();
-                    scene.setCursor(Cursor.MOVE);
-                }
-            });
-            rootLayout.setOnMouseReleased(new EventHandler<MouseEvent>() {
-
-                @Override
-                public void handle(MouseEvent mouseEvent) {
-                    scene.setCursor(Cursor.HAND);
-                    // LOGGER.info("x: " + getPrimatyStage().getX() + " " + "y: " + getPrimatyStage().getY());
-                }
-            });
-            rootLayout.setOnMouseDragged(new EventHandler<MouseEvent>() {
-
-                @Override
-                public void handle(MouseEvent mouseEvent) {
-                    secondaryStage.setX(mouseEvent.getScreenX() + dragDelta.x);
-                    secondaryStage.setY(mouseEvent.getScreenY() + dragDelta.y);
-                }
-            });
-            rootLayout.setOnMouseEntered(new EventHandler<MouseEvent>() {
-
-                @Override
-                public void handle(MouseEvent mouseEvent) {
-                    if (!mouseEvent.isPrimaryButtonDown()) {
-                        scene.setCursor(Cursor.HAND);
-                    }
-                }
-            });
-            rootLayout.setOnMouseExited(new EventHandler<MouseEvent>() {
-
-                @Override
-                public void handle(MouseEvent mouseEvent) {
-                    if (!mouseEvent.isPrimaryButtonDown()) {
-                        scene.setCursor(Cursor.DEFAULT);
-                    }
-                }
-            });
-
-        } catch (IOException e) {
-            LOGGER.error("Error loading the grid view", e);
-        }
-    }
-
-    private void createTrayIcon(final Stage stage) throws AWTException {
-        if (SystemTray.isSupported()) {
-
-            // get the SystemTray instance
-            SystemTray tray = SystemTray.getSystemTray();
-
-            // create a popup menu
-            PopupMenu popup = new PopupMenu();
-
-            MenuItem openItem = new java.awt.MenuItem("Show widget");
-            openItem.addActionListener(event -> Platform.runLater(() -> {
-
-                if (stage.isIconified()) {
-                    stage.setIconified(false);
-                    stage.show();
-                    stage.toFront();
-                }
-            }));
-
-            MenuItem hideItem = new java.awt.MenuItem("Hide widget");
-            hideItem.addActionListener(event -> Platform.runLater(() -> {
-                stage.setIconified(true);
-            }));
-
-            MenuItem exitItem = new java.awt.MenuItem("Close widget");
-            exitItem.addActionListener(event -> {
-                Platform.runLater(() -> {
-                    secondaryStage.close();
-                    getPrimatyStage().close();
-                });
-
-                System.exit(0);
-            });
-
-            popup.add(openItem);
-            popup.addSeparator();
-            popup.add(hideItem);
-            popup.addSeparator();
-            popup.add(exitItem);
-
-            trayIcon = new TrayIcon(createImage(URL_IMG_APP_16, "Icon"), "EMT Widget", popup);
-
-            /*Listener double click on trayIcon*/
-            trayIcon.addActionListener(event -> Platform.runLater(() -> {
-
-                if (stage.isIconified()) {
-                    stage.setIconified(false);
-                    stage.show();
-                    stage.toFront();
-                } else {
-                    stage.setIconified(true);
-                }
-            }));
-
-            tray.add(trayIcon);
-        }
-    }
-
-    /**
-     * Get the image for the trayIcon
-     * @param path to file
-     * @param description of the image
-     * @return a java.awt.Image object
-     */
-    private static java.awt.Image createImage(String path, String description) {
-        java.awt.Image ret = null;
-        URL imageURL = MainApp.class.getResource(path);
-
-        if (imageURL != null) {
-            ret = (new ImageIcon(imageURL, description)).getImage();
-        }
-
-        return ret;
-    }
-
-    public Stage getPrimatyStage() {
-        return this.primaryStage;
-    }
-
-    public static void main(String... varargs) {
-        launch(varargs);
-    }
+	
+	private static final Logger LOGGER = LogManager.getLogger(MainApp.class.getName());
+	private static final String TRY_ICON_16 = "/images/icon.png";
+	
+	private Stage primaryStage;
+	private Stage secondaryStage;
+	private Scene scene;
+	
+	private AnchorPane widgetView;
+	
+	private Properties properties;
+	
+	private TrayIcon trayIcon;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		LOGGER.info("Application starting");
+		
+		try {
+			// Add tryIcon
+			createTrayIcon(primaryStage);
+			
+			this.primaryStage = primaryStage;
+			this.secondaryStage = new Stage();
+			
+			primaryStage.setTitle("EMT Widget");
+			primaryStage.initStyle(StageStyle.UTILITY);
+			primaryStage.setOpacity(0);
+			primaryStage.setWidth(1);
+			primaryStage.setHeight(1);
+			
+			widgetView = FXMLLoader.load(MainApp.class.getResource("/views/widget/WidgetView.fxml"));
+			scene = new Scene(widgetView);
+			
+			loadStyleSheets();
+			loadDataFromProperties();
+			setListeners();
+			
+			secondaryStage.setScene(scene);
+			secondaryStage.initStyle(StageStyle.TRANSPARENT);
+			secondaryStage.initOwner(primaryStage);
+			
+			primaryStage.show();
+			secondaryStage.show();
+			
+		} catch (AWTException e) {
+			LOGGER.error("Error trying to create the tryicon.", e);
+		} catch (IOException e) {
+			LOGGER.error("Error trying to load the WidgetView.", e);
+		}
+	}
+	
+	private void loadStyleSheets() {
+		
+		String[] styleSheets = {
+				"/css/style.css",
+		};
+		scene.getStylesheets().addAll(styleSheets);
+	}
+	
+	private void loadDataFromProperties() {
+		
+		try {
+			properties = new Properties();
+			InputStream inputStream = getClass().getResourceAsStream("/properties/application.properties");
+			properties.load(inputStream);
+			inputStream.close();
+			
+			getPrimatyStage().setX(Integer.valueOf(properties.getProperty("widget.position.x")));
+			getPrimatyStage().setY(Integer.valueOf(properties.getProperty("widget.position.y")));
+			secondaryStage.setX(Integer.valueOf(properties.getProperty("widget.position.x")));
+			secondaryStage.setY(Integer.valueOf(properties.getProperty("widget.position.y")));
+		} catch (IOException e) {
+			LOGGER.error("Error trying to load data from properties file.", e);
+		}
+	}
+	
+	private void setListeners() {
+		
+		final Delta dragDelta = new Delta();
+		widgetView.setOnMousePressed(new EventHandler<MouseEvent>() {
+			
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				// record a delta distance for the drag and drop operation.
+				dragDelta.x = secondaryStage.getX() - mouseEvent.getScreenX();
+				dragDelta.y = secondaryStage.getY() - mouseEvent.getScreenY();
+				scene.setCursor(Cursor.MOVE);
+			}
+		});
+		widgetView.setOnMouseReleased(new EventHandler<MouseEvent>() {
+			
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				scene.setCursor(Cursor.HAND);
+				// LOGGER.info("x: " + getPrimatyStage().getX() + " " + "y: " + getPrimatyStage().getY());
+			}
+		});
+		widgetView.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				secondaryStage.setX(mouseEvent.getScreenX() + dragDelta.x);
+				secondaryStage.setY(mouseEvent.getScreenY() + dragDelta.y);
+			}
+		});
+		widgetView.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				if (!mouseEvent.isPrimaryButtonDown()) {
+					scene.setCursor(Cursor.HAND);
+				}
+			}
+		});
+		widgetView.setOnMouseExited(new EventHandler<MouseEvent>() {
+			
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				if (!mouseEvent.isPrimaryButtonDown()) {
+					scene.setCursor(Cursor.DEFAULT);
+				}
+			}
+		});
+	}
+	
+	private void createTrayIcon(final Stage stage) throws AWTException {
+		if (SystemTray.isSupported()) {
+			
+			// get the SystemTray instance
+			SystemTray tray = SystemTray.getSystemTray();
+			
+			// create a popup menu
+			PopupMenu popup = new PopupMenu();
+			
+			MenuItem openItem = new java.awt.MenuItem("Show widget");
+			openItem.addActionListener(event -> Platform.runLater(() -> {
+				
+				if (stage.isIconified()) {
+					stage.setIconified(false);
+					stage.show();
+					stage.toFront();
+				}
+			}));
+			
+			MenuItem hideItem = new java.awt.MenuItem("Hide widget");
+			hideItem.addActionListener(event -> Platform.runLater(() -> {
+				stage.setIconified(true);
+			}));
+			
+			MenuItem exitItem = new java.awt.MenuItem("Close widget");
+			exitItem.addActionListener(event -> {
+				Platform.runLater(() -> {
+					secondaryStage.close();
+					getPrimatyStage().close();
+				});
+				
+				System.exit(0);
+			});
+			
+			popup.add(openItem);
+			popup.addSeparator();
+			popup.add(hideItem);
+			popup.addSeparator();
+			popup.add(exitItem);
+			
+			trayIcon = new TrayIcon(createImage(TRY_ICON_16, "Icon"), "EMT Widget", popup);
+			
+			/* Listener simple and double click on trayIcon */
+			trayIcon.addMouseListener(new java.awt.event.MouseAdapter() {
+				
+				@Override
+				public void mouseClicked(java.awt.event.MouseEvent evt) {
+					Platform.runLater(() -> {
+						if (evt.getClickCount() == 1) {
+							// Simple
+							stage.toFront();
+						} else if (evt.getClickCount() == 2) {
+							// Double
+							if (stage.isIconified()) {
+								// Maximize it
+								stage.setIconified(false);
+								stage.show();
+								stage.toFront();
+							} else {
+								// Minimize it
+								stage.setIconified(true);
+							}
+						}
+					});
+				}
+				
+			});
+			
+			tray.add(trayIcon);
+		}
+	}
+	
+	/**
+	 * Get the image for the trayIcon
+	 * @param path to file
+	 * @param description of the image
+	 * @return a java.awt.Image object
+	 */
+	private static java.awt.Image createImage(String path, String description) {
+		java.awt.Image ret = null;
+		URL imageURL = MainApp.class.getResource(path);
+		
+		if (imageURL != null) {
+			ret = (new ImageIcon(imageURL, description)).getImage();
+		}
+		
+		return ret;
+	}
+	
+	public Stage getPrimatyStage() {
+		return this.primaryStage;
+	}
+	
+	public static void main(String... varargs) {
+		launch(varargs);
+	}
 }
 
 // records relative x and y co-ordinates.
 class Delta {
-
-    double x, y;
+	
+	double x, y;
 }
