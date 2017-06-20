8e49ebc5806ac183773f8cbf0bffb6c9e5af1115
Edgar Tamarit <edgar_pego@hotmail.com>
2017-06-20 11:08:09 +0200

    Removed the taskbar application icon.
    1 - Now the application does not show its icon in the taskbar.
    2 - ISSUE - The tray icon does not appear.

--- src/main/java/es/tamarit/widgetemt/core/MainApp.java
+++ src/main/java/es/tamarit/widgetemt/core/MainApp.java
@@ -20,120 +20,137 @@ import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
 
 public class MainApp extends Application {
-	
-	private static final Logger LOGGER = LogManager.getLogger(MainApp.class.getName());
-	
-	private Stage primaryStage;
-	private BorderPane rootLayout;
-	private Scene scene;
-	private Properties properties;
-	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		LOGGER.info("Application starting");
-		
-		this.primaryStage = primaryStage;
-		
-		primaryStage.setTitle("EMT Widget");
-		primaryStage.initStyle(StageStyle.TRANSPARENT);
-		
-		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-			public void handle(WindowEvent we) {
-				getPrimatyStage().close();
-				System.exit(0);
-			}
-		});
-		
-		initRootLayout();
-		initMainScene();
-		initMainView();
-		
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
-	
-	private void initRootLayout() {
-		try {
-			rootLayout = FXMLLoader.load(MainApp.class.getResource("/views/rootlayout/RootLayout.fxml"));
-		} catch (IOException e) {
-			LOGGER.error("Error loading the rootLayout", e);
-		}
-	}
-	
-	private void initMainScene() {
-		
-		scene = new Scene(rootLayout);
-		
-		String[] styleSheets = {
-				"/css/style.css",
-		};
-		scene.getStylesheets().addAll(styleSheets);
-	}
-	
-	private void initMainView() {
-		try {
-			AnchorPane mainView = FXMLLoader.load(MainApp.class.getResource("/views/widget/WidgetView.fxml"));
-			rootLayout.setCenter(mainView);
-			
-			properties = new Properties();
-			InputStream inputStream = getClass().getResourceAsStream("/properties/application.properties");
-			properties.load(inputStream);
-			inputStream.close();
-			
-			getPrimatyStage().setX(Integer.valueOf(properties.getProperty("widget.position.x")));
-			getPrimatyStage().setY(Integer.valueOf(properties.getProperty("widget.position.y")));
-			
-			final Delta dragDelta = new Delta();
-			rootLayout.setOnMousePressed(new EventHandler<MouseEvent>() {
-				public void handle(MouseEvent mouseEvent) {
-					// record a delta distance for the drag and drop operation.
-					dragDelta.x = getPrimatyStage().getX() - mouseEvent.getScreenX();
-					dragDelta.y = getPrimatyStage().getY() - mouseEvent.getScreenY();
-					scene.setCursor(Cursor.MOVE);
-				}
-			});
-			rootLayout.setOnMouseReleased(new EventHandler<MouseEvent>() {
-				public void handle(MouseEvent mouseEvent) {
-					scene.setCursor(Cursor.HAND);
-					// LOGGER.info("x: " + getPrimatyStage().getX() + " " + "y: " + getPrimatyStage().getY());
-				}
-			});
-			rootLayout.setOnMouseDragged(new EventHandler<MouseEvent>() {
-				public void handle(MouseEvent mouseEvent) {
-					getPrimatyStage().setX(mouseEvent.getScreenX() + dragDelta.x);
-					getPrimatyStage().setY(mouseEvent.getScreenY() + dragDelta.y);
-				}
-			});
-			rootLayout.setOnMouseEntered(new EventHandler<MouseEvent>() {
-				public void handle(MouseEvent mouseEvent) {
-					if (!mouseEvent.isPrimaryButtonDown()) {
-						scene.setCursor(Cursor.HAND);
-					}
-				}
-			});
-			rootLayout.setOnMouseExited(new EventHandler<MouseEvent>() {
-				public void handle(MouseEvent mouseEvent) {
-					if (!mouseEvent.isPrimaryButtonDown()) {
-						scene.setCursor(Cursor.DEFAULT);
-					}
-				}
-			});
-			
-		} catch (IOException e) {
-			LOGGER.error("Error loading the grid view", e);
-		}
-	}
-	
-	public static void main(String... varargs) {
-		launch(varargs);
-	}
-	
-	public Stage getPrimatyStage() {
-		return this.primaryStage;
-	}
+
+    private static final Logger LOGGER = LogManager.getLogger(MainApp.class.getName());
+
+    private Stage primaryStage;
+    private Stage secondaryStage;
+    private BorderPane rootLayout;
+    private Scene scene;
+    private Properties properties;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        LOGGER.info("Application starting");
+
+        this.primaryStage = primaryStage;
+
+        primaryStage.setTitle("EMT Widget");
+        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setOpacity(0);
+        primaryStage.setWidth(1);
+        primaryStage.setHeight(1);
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+            public void handle(WindowEvent we) {
+                getPrimatyStage().close();
+                System.exit(0);
+            }
+        });
+
+        secondaryStage = new Stage();
+
+        initRootLayout();
+        initMainScene();
+        initMainView();
+
+        secondaryStage.setScene(scene);
+        secondaryStage.initStyle(StageStyle.TRANSPARENT);
+        secondaryStage.initOwner(primaryStage);
+
+        primaryStage.show();
+        secondaryStage.show();
+    }
+
+    private void initRootLayout() {
+        try {
+            rootLayout = FXMLLoader.load(MainApp.class.getResource("/views/rootlayout/RootLayout.fxml"));
+        } catch (IOException e) {
+            LOGGER.error("Error loading the rootLayout", e);
+        }
+    }
+
+    private void initMainScene() {
+
+        scene = new Scene(rootLayout);
+
+        String[] styleSheets = { "/css/style.css", };
+        scene.getStylesheets().addAll(styleSheets);
+    }
+
+    private void initMainView() {
+        try {
+            AnchorPane mainView = FXMLLoader.load(MainApp.class.getResource("/views/widget/WidgetView.fxml"));
+            rootLayout.setCenter(mainView);
+
+            properties = new Properties();
+            InputStream inputStream = getClass().getResourceAsStream("/properties/application.properties");
+            properties.load(inputStream);
+            inputStream.close();
+
+            getPrimatyStage().setX(Integer.valueOf(properties.getProperty("widget.position.x")));
+            getPrimatyStage().setY(Integer.valueOf(properties.getProperty("widget.position.y")));
+            secondaryStage.setX(Integer.valueOf(properties.getProperty("widget.position.x")));
+            secondaryStage.setY(Integer.valueOf(properties.getProperty("widget.position.y")));
+
+            final Delta dragDelta = new Delta();
+            rootLayout.setOnMousePressed(new EventHandler<MouseEvent>() {
+
+                public void handle(MouseEvent mouseEvent) {
+                    // record a delta distance for the drag and drop operation.
+                    dragDelta.x = secondaryStage.getX() - mouseEvent.getScreenX();
+                    dragDelta.y = secondaryStage.getY() - mouseEvent.getScreenY();
+                    scene.setCursor(Cursor.MOVE);
+                }
+            });
+            rootLayout.setOnMouseReleased(new EventHandler<MouseEvent>() {
+
+                public void handle(MouseEvent mouseEvent) {
+                    scene.setCursor(Cursor.HAND);
+                    // LOGGER.info("x: " + getPrimatyStage().getX() + " " + "y: " + getPrimatyStage().getY());
+                }
+            });
+            rootLayout.setOnMouseDragged(new EventHandler<MouseEvent>() {
+
+                public void handle(MouseEvent mouseEvent) {
+                    secondaryStage.setX(mouseEvent.getScreenX() + dragDelta.x);
+                    secondaryStage.setY(mouseEvent.getScreenY() + dragDelta.y);
+                }
+            });
+            rootLayout.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+                public void handle(MouseEvent mouseEvent) {
+                    if (!mouseEvent.isPrimaryButtonDown()) {
+                        scene.setCursor(Cursor.HAND);
+                    }
+                }
+            });
+            rootLayout.setOnMouseExited(new EventHandler<MouseEvent>() {
+
+                public void handle(MouseEvent mouseEvent) {
+                    if (!mouseEvent.isPrimaryButtonDown()) {
+                        scene.setCursor(Cursor.DEFAULT);
+                    }
+                }
+            });
+
+        } catch (IOException e) {
+            LOGGER.error("Error loading the grid view", e);
+        }
+    }
+
+    public static void main(String... varargs) {
+        launch(varargs);
+    }
+
+    public Stage getPrimatyStage() {
+        return this.primaryStage;
+    }
 }
 
 // records relative x and y co-ordinates.
 class Delta {
-	double x, y;
+
+    double x, y;
 }
