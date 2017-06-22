3b21f7e4d98bdbc25c8e0324b3f61d4c0e1f23de
Edgar Tamarit <edgar_pego@hotmail.com>
2017-06-22 23:17:59 +0200

    Some UI and UX improvements
    1 - Changed the font from Roboto Slab to Lato.
    2 - Changet font size in order to show the font bigger.
    3 - Improved the settings view.
    4 - Deleted internal properties file.

--- src/main/java/es/tamarit/widgetemt/core/ViewManager.java
+++ src/main/java/es/tamarit/widgetemt/core/ViewManager.java
@@ -25,208 +25,211 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class ViewManager {
-
-    private static final Logger LOGGER = LogManager.getLogger(ViewManager.class);
-    public static final String FILE_SETTINGS = System.getProperty("user.home") + File.separator + "EMT-Widget" + File.separator + "widget.settings";
-
-    private Properties properties;
-
-    private Stage primaryStage;
-    private Stage secondaryStage;
-    private Scene scene;
-
-    private AnchorPane currentView;
-
-    public ViewManager(Stage stage) {
-
-        try {
-            checkPropertyFiles();
-
-            this.primaryStage = stage;
-            this.secondaryStage = new Stage();
-
-            primaryStage.setTitle("EMT Widget");
-            primaryStage.initStyle(StageStyle.UTILITY);
-            primaryStage.setOpacity(0);
-            primaryStage.setWidth(1);
-            primaryStage.setHeight(1);
-
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            currentView = fxmlLoader.load(getClass().getResource(WidgetController.URL_VIEW).openStream());
-            WidgetController controller = (WidgetController) fxmlLoader.getController();
-            controller.setViewManager(this);
-
-            scene = new Scene(currentView);
-
-            loadStyleSheets();
-            loadDataFromProperties();
-            setListeners();
-
-            secondaryStage.setScene(scene);
-            secondaryStage.initStyle(StageStyle.TRANSPARENT);
-            secondaryStage.initOwner(primaryStage);
-
-            primaryStage.show();
-            secondaryStage.show();
-        } catch (
-
-        IOException e) {
-            LOGGER.error("Error trying to load the WidgetView.", e);
-        }
-    }
-
-    public void loadWidgetView() {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            currentView = fxmlLoader.load(getClass().getResource(WidgetController.URL_VIEW).openStream());
-            WidgetController controller = (WidgetController) fxmlLoader.getController();
-            controller.setViewManager(this);
-
-            scene = new Scene(currentView);
-
-            loadStyleSheets();
-            setListeners();
-
-            secondaryStage.setScene(scene);
-
-        } catch (IOException e) {
-            LOGGER.error("Error trying to load the view.", e);
-        }
-    }
-
-    public void loadSettingsView() {
-
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            currentView = fxmlLoader.load(getClass().getResource(SettingsController.URL_VIEW).openStream());
-            SettingsController controller = (SettingsController) fxmlLoader.getController();
-            controller.setViewManager(this);
-
-            scene = new Scene(currentView);
-
-            loadStyleSheets();
-            setListeners();
-
-            secondaryStage.setScene(scene);
-
-        } catch (IOException e) {
-            LOGGER.error("Error trying to load the view.", e);
-        }
-    }
-
-    private void loadStyleSheets() {
-
-        String[] styleSheets = { "/css/style.css", };
-        scene.getStylesheets().addAll(styleSheets);
-    }
-
-    private void checkPropertyFiles() {
-        try {
-            File file = new File(FILE_SETTINGS);
-
-            if (!file.exists()) {
-                file.getParentFile().mkdirs();
-                file.createNewFile();
-                PrintWriter writer = new PrintWriter(file);
-                writer.println("widget.position.x=50");
-                writer.println("widget.position.y=50");
-                writer.println("bus.stop.name=824");
-                writer.close();
-            }
-        } catch (IOException e) {
-            LOGGER.error("Error trying to create the settings file.", e);
-        }
-    }
-
-    private void loadDataFromProperties() {
-
-        try {
-            properties = new Properties();
-            InputStream inputStream = new FileInputStream(FILE_SETTINGS);
-            properties.load(inputStream);
-            inputStream.close();
-
-            getPrimatyStage().setX(Double.valueOf(properties.getProperty("widget.position.x")));
-            getPrimatyStage().setY(Double.valueOf(properties.getProperty("widget.position.y")));
-            getSecondaryStage().setX(Double.valueOf(properties.getProperty("widget.position.x")));
-            getSecondaryStage().setY(Double.valueOf(properties.getProperty("widget.position.y")));
-
-        } catch (IOException e) {
-            LOGGER.error("Error trying to load data from properties file.", e);
-        }
-    }
-
-    private void setListeners() {
-
-        final Delta dragDelta = new Delta();
-        currentView.setOnMousePressed(new EventHandler<MouseEvent>() {
-
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                // record a delta distance for the drag and drop operation.
-                dragDelta.x = getSecondaryStage().getX() - mouseEvent.getScreenX();
-                dragDelta.y = getSecondaryStage().getY() - mouseEvent.getScreenY();
-                scene.setCursor(Cursor.MOVE);
-            }
-        });
-        currentView.setOnMouseReleased(new EventHandler<MouseEvent>() {
-
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                scene.setCursor(Cursor.HAND);
-                //LOGGER.info("x: " + getPrimatyStage().getX() + " " + "y: " + getPrimatyStage().getY());
-                try {
-                    OutputStream output = new FileOutputStream(FILE_SETTINGS);
-                    properties.setProperty("widget.position.x", String.valueOf(getSecondaryStage().getX()));
-                    properties.setProperty("widget.position.y", String.valueOf(getSecondaryStage().getY()));
-                    properties.store(output, null);
-                    output.close();
-                } catch (FileNotFoundException e) {
-                    LOGGER.error("Error trying open the file.", e);
-                } catch (IOException e) {
-                    LOGGER.error("Error trying to save the new data to the file.", e);
-                }
-            }
-        });
-        currentView.setOnMouseDragged(new EventHandler<MouseEvent>() {
-
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                getSecondaryStage().setX(mouseEvent.getScreenX() + dragDelta.x);
-                getSecondaryStage().setY(mouseEvent.getScreenY() + dragDelta.y);
-            }
-        });
-        currentView.setOnMouseEntered(new EventHandler<MouseEvent>() {
-
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                if (!mouseEvent.isPrimaryButtonDown()) {
-                    scene.setCursor(Cursor.HAND);
-                }
-            }
-        });
-        currentView.setOnMouseExited(new EventHandler<MouseEvent>() {
-
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                if (!mouseEvent.isPrimaryButtonDown()) {
-                    scene.setCursor(Cursor.DEFAULT);
-                }
-            }
-        });
-    }
-
-    private Stage getPrimatyStage() {
-        return this.primaryStage;
-    }
-
-    private Stage getSecondaryStage() {
-        return this.secondaryStage;
-    }
+	
+	private static final Logger LOGGER = LogManager.getLogger(ViewManager.class);
+	public static final String FILE_SETTINGS = System.getProperty("user.home") + File.separator + "EMT-Widget" + File.separator + "widget.settings";
+	
+	private Properties properties;
+	
+	private Stage primaryStage;
+	private Stage secondaryStage;
+	private Scene scene;
+	
+	private AnchorPane currentView;
+	
+	public ViewManager(Stage stage) {
+		
+		try {
+			checkPropertyFiles();
+			
+			this.primaryStage = stage;
+			this.secondaryStage = new Stage();
+			
+			primaryStage.setTitle("EMT Widget");
+			primaryStage.initStyle(StageStyle.UTILITY);
+			primaryStage.setOpacity(0);
+			primaryStage.setWidth(1);
+			primaryStage.setHeight(1);
+			
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			currentView = fxmlLoader.load(getClass().getResource(WidgetController.URL_VIEW).openStream());
+			WidgetController controller = (WidgetController) fxmlLoader.getController();
+			controller.setViewManager(this);
+			
+			scene = new Scene(currentView);
+			
+			loadStyleSheets();
+			loadDataFromProperties();
+			setListeners();
+			
+			secondaryStage.setScene(scene);
+			secondaryStage.initStyle(StageStyle.TRANSPARENT);
+			secondaryStage.initOwner(primaryStage);
+			
+			primaryStage.show();
+			secondaryStage.show();
+		} catch (
+		
+		IOException e) {
+			LOGGER.error("Error trying to load the WidgetView.", e);
+		}
+	}
+	
+	public void loadWidgetView() {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			currentView = fxmlLoader.load(getClass().getResource(WidgetController.URL_VIEW).openStream());
+			WidgetController controller = (WidgetController) fxmlLoader.getController();
+			controller.setViewManager(this);
+			
+			scene = new Scene(currentView);
+			
+			loadStyleSheets();
+			setListeners();
+			
+			secondaryStage.setScene(scene);
+			
+		} catch (IOException e) {
+			LOGGER.error("Error trying to load the view.", e);
+		}
+	}
+	
+	public void loadSettingsView() {
+		
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			currentView = fxmlLoader.load(getClass().getResource(SettingsController.URL_VIEW).openStream());
+			SettingsController controller = (SettingsController) fxmlLoader.getController();
+			controller.setViewManager(this);
+			
+			scene = new Scene(currentView);
+			
+			loadStyleSheets();
+			setListeners();
+			
+			secondaryStage.setScene(scene);
+			
+		} catch (IOException e) {
+			LOGGER.error("Error trying to load the view.", e);
+		}
+	}
+	
+	private void loadStyleSheets() {
+		
+		String[] styleSheets = {
+				"/css/style.css",
+				"https://fonts.googleapis.com/css?family=Lato"
+		};
+		scene.getStylesheets().addAll(styleSheets);
+	}
+	
+	private void checkPropertyFiles() {
+		try {
+			File file = new File(FILE_SETTINGS);
+			
+			if (!file.exists()) {
+				file.getParentFile().mkdirs();
+				file.createNewFile();
+				PrintWriter writer = new PrintWriter(file);
+				writer.println("widget.position.x=50");
+				writer.println("widget.position.y=50");
+				writer.println("bus.stop.name=824");
+				writer.close();
+			}
+		} catch (IOException e) {
+			LOGGER.error("Error trying to create the settings file.", e);
+		}
+	}
+	
+	private void loadDataFromProperties() {
+		
+		try {
+			properties = new Properties();
+			InputStream inputStream = new FileInputStream(FILE_SETTINGS);
+			properties.load(inputStream);
+			inputStream.close();
+			
+			getPrimatyStage().setX(Double.valueOf(properties.getProperty("widget.position.x")));
+			getPrimatyStage().setY(Double.valueOf(properties.getProperty("widget.position.y")));
+			getSecondaryStage().setX(Double.valueOf(properties.getProperty("widget.position.x")));
+			getSecondaryStage().setY(Double.valueOf(properties.getProperty("widget.position.y")));
+			
+		} catch (IOException e) {
+			LOGGER.error("Error trying to load data from properties file.", e);
+		}
+	}
+	
+	private void setListeners() {
+		
+		final Delta dragDelta = new Delta();
+		currentView.setOnMousePressed(new EventHandler<MouseEvent>() {
+			
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				// record a delta distance for the drag and drop operation.
+				dragDelta.x = getSecondaryStage().getX() - mouseEvent.getScreenX();
+				dragDelta.y = getSecondaryStage().getY() - mouseEvent.getScreenY();
+				scene.setCursor(Cursor.MOVE);
+			}
+		});
+		currentView.setOnMouseReleased(new EventHandler<MouseEvent>() {
+			
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				scene.setCursor(Cursor.HAND);
+				// LOGGER.info("x: " + getPrimatyStage().getX() + " " + "y: " + getPrimatyStage().getY());
+				try {
+					OutputStream output = new FileOutputStream(FILE_SETTINGS);
+					properties.setProperty("widget.position.x", String.valueOf(getSecondaryStage().getX()));
+					properties.setProperty("widget.position.y", String.valueOf(getSecondaryStage().getY()));
+					properties.store(output, null);
+					output.close();
+				} catch (FileNotFoundException e) {
+					LOGGER.error("Error trying open the file.", e);
+				} catch (IOException e) {
+					LOGGER.error("Error trying to save the new data to the file.", e);
+				}
+			}
+		});
+		currentView.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				getSecondaryStage().setX(mouseEvent.getScreenX() + dragDelta.x);
+				getSecondaryStage().setY(mouseEvent.getScreenY() + dragDelta.y);
+			}
+		});
+		currentView.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				if (!mouseEvent.isPrimaryButtonDown()) {
+					scene.setCursor(Cursor.HAND);
+				}
+			}
+		});
+		currentView.setOnMouseExited(new EventHandler<MouseEvent>() {
+			
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				if (!mouseEvent.isPrimaryButtonDown()) {
+					scene.setCursor(Cursor.DEFAULT);
+				}
+			}
+		});
+	}
+	
+	private Stage getPrimatyStage() {
+		return this.primaryStage;
+	}
+	
+	private Stage getSecondaryStage() {
+		return this.secondaryStage;
+	}
 }
 
 // records relative x and y co-ordinates.
 class Delta {
-
-    double x, y;
+	
+	double x, y;
 }
--- src/main/java/es/tamarit/widgetemt/settings/SettingsController.java
+++ src/main/java/es/tamarit/widgetemt/settings/SettingsController.java
@@ -31,147 +31,150 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 public class SettingsController {
-
-    private static final Logger LOGGER = LogManager.getLogger(SettingsController.class);
-    public static final String URL_VIEW = "/views/settings/SettingsView.fxml";
-
-    private ViewManager viewManager;
-    private String response;
-
-    @FXML
-    private TextField textField;
-    @FXML
-    private ComboBox<String> busStopCombo;
-
-    @FXML
-    public void initialize() {
-
-        LOGGER.info("Settings");
-        textField.textProperty().addListener((ChangeListener<String>) (arg0, arg1, arg2) -> textEditor(arg1, arg2));
-
-        Platform.runLater(() -> {
-            textField.requestFocus();
-        });
-    }
-
-    @FXML
-    private void openWidgetViewConfirm() {
-
-        if (busStopCombo.getValue() == null) {
-            openWidgetViewCancel();
-        } else {
-            if (busStopCombo.getValue().isEmpty()) {
-                openWidgetViewCancel();
-            } else {
-                try {
-                    Properties properties = new Properties();
-                    InputStream inputStream = new FileInputStream(ViewManager.FILE_SETTINGS);
-                    properties.load(inputStream);
-                    inputStream.close();
-
-                    OutputStream output = new FileOutputStream(ViewManager.FILE_SETTINGS);
-                    properties.setProperty("bus.stop.name", busStopCombo.getValue());
-                    properties.store(output, null);
-                    output.close();
-                } catch (FileNotFoundException e) {
-                    LOGGER.error("Error trying open the file.", e);
-                } catch (IOException e) {
-                    LOGGER.error("Error trying to save the new data to the file.", e);
-                }
-
-                viewManager.loadWidgetView();
-            }
-        }
-    }
-
-    @FXML
-    private void openWidgetViewCancel() {
-        viewManager.loadWidgetView();
-    }
-
-    private void textEditor(String textBefore, String textAfter) {
-
-        busStopCombo.getItems().clear();
-        String text = textAfter;
-
-        if (!text.isEmpty()) {
-
-            new Thread(() -> {
-                try {
-                    if (Character.isDigit(text.charAt(0))) {
-                        response = getResponse("id_parada", text);
-
-                    } else if (Character.isLetter(text.charAt(0))) {
-                        response = getResponse("parada", text);
-                    }
-
-                } catch (IOException e) {
-                    LOGGER.error("Error trying to get the response from the EMT server", e);
-                } finally {
-
-                    Platform.runLater(() -> {
-                        if (response != null) {
-                            //LOGGER.info(response);
-                            Document doc = Jsoup.parse(response);
-                            Elements elements = doc.getElementsByTag("li");
-                            for (Element element : elements) {
-                                //LOGGER.info(element.text());
-                                busStopCombo.getItems().add(element.text());
-                            }
-
-                            busStopCombo.hide();
-                            busStopCombo.setVisibleRowCount(busStopCombo.getItems().size());
-                            busStopCombo.show();
-                        }
-                    });
-                }
-            }).start();
-        }
-    }
-
-    private String getResponse(String tag, String stopName) throws IOException {
-        URL url = new URL("https://www.emtvalencia.es/ciudadano/modules/mod_tiempo/sugiere_parada.php"); // URL to your application
-        Map<String, String> params = new LinkedHashMap<String, String>();
-        params.put(tag, stopName); // All parameters, also easy
-
-        StringBuilder postData = new StringBuilder();
-        // POST as URL encoded is basically key-value pairs, as with GET
-        // This creates key=value&key=value&... pairs
-        for (Map.Entry<String, String> param : params.entrySet()) {
-            if (postData.length() != 0)
-                postData.append('&');
-            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
-            postData.append('=');
-            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
-        }
-
-        // Convert string to byte array, as it should be sent
-        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
-
-        // Connect, easy
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        // Tell server that this is POST and in which format is the data
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
-        conn.setDoOutput(true);
-        conn.getOutputStream().write(postDataBytes);
-
-        // This gets the output from your server
-        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-
-        StringBuilder sb = new StringBuilder();
-        for (int c; (c = in.read()) >= 0;) {
-            sb.append((char) c);
-            // System.out.print((char) c);
-        }
-
-        in.close();
-
-        return sb.toString();
-    }
-
-    public void setViewManager(ViewManager viewManager) {
-        this.viewManager = viewManager;
-    }
+	
+	private static final Logger LOGGER = LogManager.getLogger(SettingsController.class);
+	public static final String URL_VIEW = "/views/settings/SettingsView.fxml";
+	
+	private ViewManager viewManager;
+	private String response;
+	
+	@FXML
+	private TextField textField;
+	@FXML
+	private ComboBox<String> busStopCombo;
+	
+	@FXML
+	public void initialize() {
+		LOGGER.info("Settings");
+		
+		textField.textProperty().addListener((ChangeListener<String>) (arg0, arg1, arg2) -> textEditor(arg1, arg2));
+	}
+	
+	@FXML
+	private void openWidgetViewConfirm() {
+		
+		if (busStopCombo.getValue() == null) {
+			openWidgetViewCancel();
+		} else {
+			if (busStopCombo.getValue().isEmpty()) {
+				openWidgetViewCancel();
+			} else {
+				try {
+					Properties properties = new Properties();
+					InputStream inputStream = new FileInputStream(ViewManager.FILE_SETTINGS);
+					properties.load(inputStream);
+					inputStream.close();
+					
+					OutputStream output = new FileOutputStream(ViewManager.FILE_SETTINGS);
+					properties.setProperty("bus.stop.name", busStopCombo.getValue());
+					properties.store(output, null);
+					output.close();
+				} catch (FileNotFoundException e) {
+					LOGGER.error("Error trying open the file.", e);
+				} catch (IOException e) {
+					LOGGER.error("Error trying to save the new data to the file.", e);
+				}
+				
+				viewManager.loadWidgetView();
+			}
+		}
+	}
+	
+	@FXML
+	private void openWidgetViewCancel() {
+		viewManager.loadWidgetView();
+	}
+	
+	private void textEditor(String textBefore, String textAfter) {
+		
+		busStopCombo.getItems().clear();
+		String text = textAfter;
+		
+		if (!text.isEmpty()) {
+			
+			new Thread(() -> {
+				try {
+					if (Character.isDigit(text.charAt(0))) {
+						response = getResponse("id_parada", text);
+						
+					} else if (Character.isLetter(text.charAt(0))) {
+						response = getResponse("parada", text);
+					}
+					
+				} catch (IOException e) {
+					LOGGER.error("Error trying to get the response from the EMT server", e);
+				} finally {
+					
+					Platform.runLater(() -> {
+						if (response != null) {
+							// LOGGER.info(response);
+							Document doc = Jsoup.parse(response);
+							Elements elements = doc.getElementsByTag("li");
+							for (Element element : elements) {
+								// LOGGER.info(element.text());
+								busStopCombo.getItems().add(element.text());
+							}
+							
+							busStopCombo.setVisible(false);
+							busStopCombo.hide();
+							if (!busStopCombo.getItems().isEmpty()) {
+								busStopCombo.setVisibleRowCount(busStopCombo.getItems().size());
+								busStopCombo.setVisible(true);
+								busStopCombo.show();
+							}
+						}
+					});
+				}
+			}).start();
+		} else {
+			busStopCombo.hide();
+			busStopCombo.setVisible(false);
+		}
+	}
+	
+	private String getResponse(String tag, String stopName) throws IOException {
+		URL url = new URL("https://www.emtvalencia.es/ciudadano/modules/mod_tiempo/sugiere_parada.php"); // URL to your application
+		Map<String, String> params = new LinkedHashMap<String, String>();
+		params.put(tag, stopName); // All parameters, also easy
+		
+		StringBuilder postData = new StringBuilder();
+		// POST as URL encoded is basically key-value pairs, as with GET
+		// This creates key=value&key=value&... pairs
+		for (Map.Entry<String, String> param : params.entrySet()) {
+			if (postData.length() != 0)
+				postData.append('&');
+			postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+			postData.append('=');
+			postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+		}
+		
+		// Convert string to byte array, as it should be sent
+		byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+		
+		// Connect, easy
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		// Tell server that this is POST and in which format is the data
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+		conn.setDoOutput(true);
+		conn.getOutputStream().write(postDataBytes);
+		
+		// This gets the output from your server
+		Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+		
+		StringBuilder sb = new StringBuilder();
+		for (int c; (c = in.read()) >= 0;) {
+			sb.append((char) c);
+			// System.out.print((char) c);
+		}
+		
+		in.close();
+		
+		return sb.toString();
+	}
+	
+	public void setViewManager(ViewManager viewManager) {
+		this.viewManager = viewManager;
+	}
 }
