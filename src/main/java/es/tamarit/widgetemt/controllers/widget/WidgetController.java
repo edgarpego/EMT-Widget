c521b77a3f2179163931deeaf2b3dd193d2a29cf
Edgar Tamarit <edgar_pego@hotmail.com>
2017-07-05 08:14:57 +0200

    Version 3.1.1
    1 - Bug fixed which causes the wrong auto refresh the times data.

--- src/main/java/es/tamarit/widgetemt/controllers/widget/WidgetController.java
+++ src/main/java/es/tamarit/widgetemt/controllers/widget/WidgetController.java
@@ -31,34 +31,34 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 public class WidgetController extends AbstractController {
-    
+
     private static final Logger LOGGER = LogManager.getLogger(WidgetController.class);
     public static final String URL_VIEW = "/views/widget/WidgetView.fxml";
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-    
+
     @FXML
     private WebView myWebView;
     @FXML
     private ProgressIndicator progress;
     @FXML
     private Button refreshButton;
-    
+
     private WebEngine myWebEngine;
     private Properties properties;
     private String response;
-    
+
     @FXML
     public void initialize() {
         try {
             myWebView.setContextMenuEnabled(false);
             myWebEngine = myWebView.getEngine();
             myWebEngine.setUserStyleSheetLocation(getClass().getResource("/css/webstyle.css").toString());
-            
+
             properties = new Properties();
             InputStream inputStream = new FileInputStream(ViewManager.FILE_SETTINGS);
             properties.load(inputStream);
             inputStream.close();
-            
+
             if (Boolean.valueOf(properties.get("auto.refresh.data").toString()) == Boolean.TRUE) {
                 scheduler.scheduleAtFixedRate(() -> printTimes(), 0, 30, TimeUnit.SECONDS);
             } else {
@@ -70,36 +70,36 @@ public class WidgetController extends AbstractController {
             LOGGER.error("Error trying to load the properties", e);
         }
     }
-    
+
     @FXML
     private void reloadHandler() {
         printTimes();
     }
-    
+
     private void printTimes() {
         LOGGER.info("Printing the times");
-        
+
         Platform.runLater(() -> {
             progress.setVisible(true);
             refreshButton.setDisable(true);
             myWebEngine.loadContent("");
         });
-        
+
         new Thread(() -> {
             try {
                 String stopName = properties.getProperty("bus.stop.name");
                 Locale locale = Locale.forLanguageTag(properties.getProperty("application.language.locale"));
                 String language = locale.getLanguage();
-                
+
                 response = getResponse(stopName, language);
-                
+
             } catch (IOException e) {
                 LOGGER.error("Error trying to get the response from the EMT server", e);
             } finally {
-                
+
                 Platform.runLater(() -> {
                     if (response != null && !response.isEmpty()) {
-                        if (response.contains("No disponible. Actualiza en unos segundos.")) {
+                        if (response.contains("No disponible") || response.contains("Out of service")) {
                             printTimes();
                         } else {
                             myWebEngine.loadContent("<html><body>" + response + "</body></html>");
@@ -115,7 +115,7 @@ public class WidgetController extends AbstractController {
             }
         }).start();
     }
-    
+
     private String getResponse(String stopName, String language) throws IOException {
         URL url = new URL("https://www.emtvalencia.es/ciudadano/modules/mod_tiempo/busca_parada.php"); // URL to your application
         Map<String, String> params = new LinkedHashMap<String, String>();
@@ -123,7 +123,7 @@ public class WidgetController extends AbstractController {
         params.put("adaptados", "0");
         params.put("usuario", "Anonimo");
         params.put("idioma", language);
-        
+
         StringBuilder postData = new StringBuilder();
         // POST as URL encoded is basically key-value pairs, as with GET
         // This creates key=value&key=value&... pairs
@@ -134,10 +134,10 @@ public class WidgetController extends AbstractController {
             postData.append('=');
             postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
         }
-        
+
         // Convert string to byte array, as it should be sent
         byte[] postDataBytes = postData.toString().getBytes("UTF-8");
-        
+
         // Connect, easy
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         // Tell server that this is POST and in which format is the data
@@ -146,21 +146,21 @@ public class WidgetController extends AbstractController {
         conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
         conn.setDoOutput(true);
         conn.getOutputStream().write(postDataBytes);
-        
+
         // This gets the output from your server
         Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-        
+
         StringBuilder sb = new StringBuilder();
         for (int c; (c = in.read()) >= 0;) {
             sb.append((char) c);
             // System.out.print((char) c);
         }
-        
+
         in.close();
-        
+
         return sb.toString();
     }
-    
+
     @FXML
     private void openSettingsWindow() {
         scheduler.shutdown();
