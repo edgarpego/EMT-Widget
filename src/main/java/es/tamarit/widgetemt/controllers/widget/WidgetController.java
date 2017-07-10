74d9dda1e9faff9e5a0c204c1fa35ea329f287c9
Edgar Tamarit <edgar_pego@hotmail.com>
2017-07-10 12:43:05 +0200

    Minor improvements
    1 - Minor bug fixes on widgetView.
    2 - Added all controll panel settings but some of them without
    functionality yet.
    3 - Added new images and CSS components.

--- src/main/java/es/tamarit/widgetemt/controllers/widget/WidgetController.java
+++ src/main/java/es/tamarit/widgetemt/controllers/widget/WidgetController.java
@@ -25,45 +25,49 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 public class WidgetController extends AbstractController {
-    
+
     private static final Logger LOGGER = LogManager.getLogger(WidgetController.class);
     public static final String URL_VIEW = "/views/widget/WidgetView.fxml";
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-    
+
     @FXML
     private WebView myWebView;
     @FXML
     private ProgressIndicator progress;
     @FXML
     private Button refreshButton;
-    
+
     private WebEngine myWebEngine;
     private Properties properties;
     private String response;
     private StopTimes stopTimes;
-    
+
     @FXML
     public void initialize() {
         try {
-            myWebView.setContextMenuEnabled(false);
-            myWebEngine = myWebView.getEngine();
-            myWebEngine.setUserStyleSheetLocation(getClass().getResource("/css/webstyle.css").toString());
-            
             properties = new Properties();
             InputStream inputStream = new FileInputStream(ViewManager.FILE_SETTINGS);
             properties.load(inputStream);
             inputStream.close();
-            
+
             String stopName = properties.getProperty("bus.stop.name");
-            Locale locale = Locale.forLanguageTag(properties.getProperty("application.language.locale"));
-            String language = locale.getLanguage();
-            
-            stopTimes = new StopTimesImpl(stopName, "none", "0", language);
-            
-            if (Boolean.valueOf(properties.get("auto.refresh.data").toString()) == Boolean.TRUE) {
-                scheduler.scheduleAtFixedRate(() -> printTimes(), 0, 30, TimeUnit.SECONDS);
-            } else {
-                printTimes();
+
+            if (stopName != null && !stopName.isEmpty()) {
+
+                myWebView.setContextMenuEnabled(false);
+                myWebEngine = myWebView.getEngine();
+                myWebEngine.setUserStyleSheetLocation(getClass().getResource("/css/webstyle.css").toString());
+
+                Locale locale = Locale.forLanguageTag(properties.getProperty("application.language.locale"));
+                String language = locale.getLanguage();
+
+                stopTimes = new StopTimesImpl(stopName, "none", "0", language);
+
+                if (Boolean.valueOf(properties.get("auto.refresh.data").toString()) == Boolean.TRUE) {
+                    scheduler.scheduleAtFixedRate(() -> printTimes(), 0, 30, TimeUnit.SECONDS);
+                } else {
+                    printTimes();
+                }
             }
         } catch (FileNotFoundException e) {
             LOGGER.error("Error trying to get the properties file", e);
@@ -71,49 +75,52 @@ public class WidgetController extends AbstractController {
             LOGGER.error("Error trying to load the properties", e);
         }
     }
-    
+
     @FXML
     private void reloadHandler() {
         printTimes();
     }
-    
+
     private void printTimes() {
         LOGGER.info("Printing the times");
-        
-        Platform.runLater(() -> {
-            progress.setVisible(true);
-            refreshButton.setDisable(true);
-            myWebEngine.loadContent("");
-        });
-        
-        new Thread(() -> {
-            try {
-                
-                response = stopTimes.getStopTimes();
-                
-            } catch (IOException e) {
-                LOGGER.error("Error trying to get the response from the EMT server", e);
-            } finally {
-                
-                Platform.runLater(() -> {
-                    if (response != null && !response.isEmpty()) {
-                        if (response.contains("No disponible") || response.contains("Out of service")) {
-                            printTimes();
+
+        if (stopTimes != null) {
+
+            Platform.runLater(() -> {
+                progress.setVisible(true);
+                refreshButton.setDisable(true);
+                myWebEngine.loadContent("");
+            });
+
+            new Thread(() -> {
+                try {
+
+                    response = stopTimes.getStopTimes();
+
+                } catch (IOException e) {
+                    LOGGER.error("Error trying to get the response from the EMT server", e);
+                } finally {
+
+                    Platform.runLater(() -> {
+                        if (response != null && !response.isEmpty()) {
+                            if (response.contains("No disponible") || response.contains("Out of service")) {
+                                printTimes();
+                            } else {
+                                myWebEngine.loadContent("<html><body>" + response + "</body></html>");
+                                refreshButton.setDisable(false);
+                                progress.setVisible(false);
+                            }
                         } else {
-                            myWebEngine.loadContent("<html><body>" + response + "</body></html>");
+                            myWebEngine.loadContent("<html><body><h3>There was an error, Try again in a few seconds!</h3></body></html>");
                             refreshButton.setDisable(false);
                             progress.setVisible(false);
                         }
-                    } else {
-                        myWebEngine.loadContent("<html><body><h3>There was an error, Try again in a few seconds!</h3></body></html>");
-                        refreshButton.setDisable(false);
-                        progress.setVisible(false);
-                    }
-                });
-            }
-        }).start();
+                    });
+                }
+            }).start();
+        }
     }
-    
+
     @FXML
     private void openSettingsWindow() {
         scheduler.shutdown();
--- src/main/java/es/tamarit/widgetemt/core/ViewManager.java
+++ src/main/java/es/tamarit/widgetemt/core/ViewManager.java
@@ -27,87 +27,86 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class ViewManager {
-    
+
     private static final Logger LOGGER = LogManager.getLogger(ViewManager.class);
     public static final String FILE_SETTINGS = System.getProperty("user.home") + File.separator + ".EMTWidget" + File.separator + "settings.emt";
-    
+
     private Properties properties;
-    
+
     private Stage primaryStage;
     private Stage secondaryStage;
     private Scene scene;
     private AnchorPane currentView;
-    
+
     public ViewManager(Stage stage) {
-        
+
         this.primaryStage = stage;
         this.secondaryStage = new Stage();
-        
+
         primaryStage.setTitle("EMT Widget");
         primaryStage.initStyle(StageStyle.UTILITY);
         primaryStage.setOpacity(0);
         primaryStage.setWidth(1);
         primaryStage.setHeight(1);
-        
+
         loadWidgetView();
-        
+
         primaryStage.setX(Double.valueOf(properties.getProperty("widget.position.x")));
         primaryStage.setY(Double.valueOf(properties.getProperty("widget.position.y")));
         secondaryStage.setX(Double.valueOf(properties.getProperty("widget.position.x")));
         secondaryStage.setY(Double.valueOf(properties.getProperty("widget.position.y")));
         secondaryStage.initStyle(StageStyle.TRANSPARENT);
         secondaryStage.initOwner(primaryStage);
-        
+
         primaryStage.show();
         secondaryStage.show();
     }
-    
+
     public void loadWidgetView() {
         try {
             loadDataFromProperties();
-            
+
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setResources(getResourceBundle());
             currentView = fxmlLoader.load(getClass().getResource(WidgetController.URL_VIEW).openStream());
             WidgetController controller = (WidgetController) fxmlLoader.getController();
             controller.setViewManager(this);
-            
+
             scene = new Scene(currentView);
-            
+
             loadStyleSheets();
             setListeners();
-            
+
             secondaryStage.setScene(scene);
             secondaryStage.setAlwaysOnTop(Boolean.valueOf(properties.get("always.on.front").toString()));
-            
+
         } catch (IOException e) {
             LOGGER.error("Error trying to load the view.", e);
         }
     }
-    
+
     public void loadSettingsView() {
-        
+
         try {
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setResources(getResourceBundle());
             currentView = fxmlLoader.load(getClass().getResource(SettingsController.URL_VIEW).openStream());
             SettingsController controller = (SettingsController) fxmlLoader.getController();
             controller.setViewManager(this);
-            
+
             scene = new Scene(currentView);
-            
+
             loadStyleSheets();
-            setListeners();
-            
+
             secondaryStage.setScene(scene);
-            
+
         } catch (IOException e) {
             LOGGER.error("Error trying to load the view.", e);
         }
     }
-    
+
     private void loadStyleSheets() {
-        
+
         String[] styleSheets = {
                 "/css/style.css",
                 "/css/components.css",
@@ -115,29 +114,29 @@ public class ViewManager {
         };
         scene.getStylesheets().addAll(styleSheets);
     }
-    
+
     private void loadDataFromProperties() {
-        
+
         try {
             newFileIfNotExists();
-            
+
             properties = new Properties();
             InputStream inputStream = new FileInputStream(FILE_SETTINGS);
             properties.load(inputStream);
             inputStream.close();
-            
+
             checkPropertiesIntegrity();
-            
+
         } catch (IOException e) {
             LOGGER.error("Error trying to load data from properties file.", e);
         }
     }
-    
+
     private void checkPropertiesIntegrity() {
-        
+
         try {
             boolean neededRestore = false;
-            
+
             if (properties.get("widget.position.x") == null) {
                 properties.setProperty("widget.position.x", "50");
                 neededRestore = true;
@@ -162,24 +161,24 @@ public class ViewManager {
                 properties.setProperty("application.language.locale", "es-ES");
                 neededRestore = true;
             }
-            
+
             if (neededRestore) {
                 OutputStream output = new FileOutputStream(ViewManager.FILE_SETTINGS);
                 properties.store(output, null);
                 output.close();
             }
-            
+
         } catch (FileNotFoundException e) {
             LOGGER.error("Error trying open the file.", e);
         } catch (IOException e) {
             LOGGER.error("Error trying to save the new data to the file.", e);
         }
     }
-    
+
     private void newFileIfNotExists() {
         try {
             File file = new File(FILE_SETTINGS);
-            
+
             if (!file.exists()) {
                 file.getParentFile().mkdirs();
                 file.createNewFile();
@@ -196,35 +195,36 @@ public class ViewManager {
             LOGGER.error("Error trying to create the settings file.", e);
         }
     }
-    
+
     private ResourceBundle getResourceBundle() {
-        
+
         Locale locale = Locale.forLanguageTag(properties.getProperty("application.language.locale"));
         LanguageManager languageManager = new LanguageManager(locale);
-        
+
         return languageManager.getResourceBundle();
     }
-    
+
     private void setListeners() {
-        
+
         final Delta dragDelta = new Delta();
         currentView.setOnMousePressed(new EventHandler<MouseEvent>() {
-            
+
             @Override
             public void handle(MouseEvent mouseEvent) {
+                scene.setCursor(Cursor.MOVE);
+
                 // record a delta distance for the drag and drop operation.
                 dragDelta.x = getSecondaryStage().getX() - mouseEvent.getScreenX();
                 dragDelta.y = getSecondaryStage().getY() - mouseEvent.getScreenY();
-                scene.setCursor(Cursor.MOVE);
             }
         });
         currentView.setOnMouseReleased(new EventHandler<MouseEvent>() {
-            
+
             @Override
             public void handle(MouseEvent mouseEvent) {
-                scene.setCursor(Cursor.HAND);
-                // LOGGER.info("x: " + getPrimatyStage().getX() + " " + "y: " + getPrimatyStage().getY());
                 try {
+                    scene.setCursor(Cursor.HAND);
+
                     OutputStream output = new FileOutputStream(FILE_SETTINGS);
                     properties.setProperty("widget.position.x", String.valueOf(getSecondaryStage().getX()));
                     properties.setProperty("widget.position.y", String.valueOf(getSecondaryStage().getY()));
@@ -238,7 +238,7 @@ public class ViewManager {
             }
         });
         currentView.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            
+
             @Override
             public void handle(MouseEvent mouseEvent) {
                 getSecondaryStage().setX(mouseEvent.getScreenX() + dragDelta.x);
@@ -246,7 +246,7 @@ public class ViewManager {
             }
         });
         currentView.setOnMouseEntered(new EventHandler<MouseEvent>() {
-            
+
             @Override
             public void handle(MouseEvent mouseEvent) {
                 if (!mouseEvent.isPrimaryButtonDown()) {
@@ -255,7 +255,7 @@ public class ViewManager {
             }
         });
         currentView.setOnMouseExited(new EventHandler<MouseEvent>() {
-            
+
             @Override
             public void handle(MouseEvent mouseEvent) {
                 if (!mouseEvent.isPrimaryButtonDown()) {
@@ -264,7 +264,7 @@ public class ViewManager {
             }
         });
     }
-    
+
     private Stage getSecondaryStage() {
         return this.secondaryStage;
     }
@@ -272,6 +272,6 @@ public class ViewManager {
 
 // records relative x and y co-ordinates.
 class Delta {
-    
+
     double x, y;
 }
