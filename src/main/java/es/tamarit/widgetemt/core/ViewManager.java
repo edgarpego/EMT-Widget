abcbff1ffb0682a0d60e852ef31a08ca04dd56b3
Edgar Tamarit <edgar_pego@hotmail.com>
2017-07-11 12:18:56 +0200

    Multilanguage on settings view
    1 - Now the new settings view supports multilanguage.
    2 - Minor bug fixes when try to save the settings values.

--- src/main/java/es/tamarit/widgetemt/core/ViewManager.java
+++ src/main/java/es/tamarit/widgetemt/core/ViewManager.java
@@ -19,88 +19,81 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class ViewManager {
-    
+
     private static final Logger LOGGER = LogManager.getLogger(ViewManager.class);
-    
+
     private Stage primaryStage;
     private Stage secondaryStage;
     private Scene scene;
     private AnchorPane currentView;
     private FilePropertiesService properties;
-    
+
     public ViewManager(Stage stage) {
-        
-        try {
-            properties = new SettingsPropertiesServiceImpl();
-            
-            this.primaryStage = stage;
-            this.secondaryStage = new Stage();
-            
-            primaryStage.setTitle("EMT Widget");
-            primaryStage.initStyle(StageStyle.UTILITY);
-            primaryStage.setOpacity(0);
-            primaryStage.setWidth(1);
-            primaryStage.setHeight(1);
-            
-            loadWidgetView();
-            
-            primaryStage.setX(Double.valueOf(properties.getProperty("widget.position.x")));
-            primaryStage.setY(Double.valueOf(properties.getProperty("widget.position.y")));
-            secondaryStage.setX(Double.valueOf(properties.getProperty("widget.position.x")));
-            secondaryStage.setY(Double.valueOf(properties.getProperty("widget.position.y")));
-            secondaryStage.initStyle(StageStyle.TRANSPARENT);
-            secondaryStage.initOwner(primaryStage);
-            
-            primaryStage.show();
-            secondaryStage.show();
-            
-        } catch (IOException e) {
-            LOGGER.error("Error trying to load the properties", e);
-        }
+
+        this.primaryStage = stage;
+        this.secondaryStage = new Stage();
+
+        primaryStage.setTitle("EMT Widget");
+        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setOpacity(0);
+        primaryStage.setWidth(1);
+        primaryStage.setHeight(1);
+
+        loadWidgetView();
+
+        primaryStage.setX(Double.valueOf(properties.getProperty("widget.position.x")));
+        primaryStage.setY(Double.valueOf(properties.getProperty("widget.position.y")));
+        secondaryStage.setX(Double.valueOf(properties.getProperty("widget.position.x")));
+        secondaryStage.setY(Double.valueOf(properties.getProperty("widget.position.y")));
+        secondaryStage.initStyle(StageStyle.TRANSPARENT);
+        secondaryStage.initOwner(primaryStage);
+
+        primaryStage.show();
+        secondaryStage.show();
     }
-    
+
     public void loadWidgetView() {
-        
+
         try {
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setResources(getResourceBundle());
             currentView = fxmlLoader.load(getClass().getResource(WidgetController.URL_VIEW).openStream());
             WidgetController controller = (WidgetController) fxmlLoader.getController();
             controller.setViewManager(this);
-            
+
             scene = new Scene(currentView);
-            
+
             loadStyleSheets();
-            
+
             secondaryStage.setScene(scene);
-            
+
         } catch (IOException e) {
             LOGGER.error("Error trying to load the view.", e);
         }
     }
-    
+
     public void loadSettingsView() {
-        
+
         try {
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setResources(getResourceBundle());
             currentView = fxmlLoader.load(getClass().getResource(SettingsController.URL_VIEW).openStream());
             SettingsController controller = (SettingsController) fxmlLoader.getController();
             controller.setViewManager(this);
-            
+
             scene = new Scene(currentView);
-            
+
             loadStyleSheets();
-            
+
             secondaryStage.setScene(scene);
-            
+
         } catch (IOException e) {
             LOGGER.error("Error trying to load the view.", e);
         }
     }
-    
+
     private void loadStyleSheets() {
-        
+
         String[] styleSheets = {
                 "/css/style.css",
                 "/css/components.css",
@@ -108,15 +101,23 @@ public class ViewManager {
         };
         scene.getStylesheets().addAll(styleSheets);
     }
-    
+
     private ResourceBundle getResourceBundle() {
-        
-        Locale locale = Locale.forLanguageTag(properties.getProperty("application.language.locale"));
-        LanguageManager languageManager = new LanguageManager(locale);
-        
-        return languageManager.getResourceBundle();
+        try {
+            properties = new SettingsPropertiesServiceImpl();
+
+            Locale locale = Locale.forLanguageTag(properties.getProperty("application.language.locale"));
+            LanguageManager languageManager = new LanguageManager(locale);
+
+            return languageManager.getResourceBundle();
+
+        } catch (IOException e) {
+            LOGGER.error("Error trying to load the properties", e);
+        }
+
+        return null;
     }
-    
+
     public Stage getSecondaryStage() {
         return this.secondaryStage;
     }
